d1f4fac7cd2e58ec34bd2c1df2b9cb03c2dcea89
Onike <taofeek.onike@yahoo.com>
2019-09-01 10:58:43 +0300

    Training

--- Optional/OptionalExample5.java
+++ Optional/OptionalExample5.java
@@ -13,17 +13,23 @@ public class OptionalExample5 {
 
         // It returns a non-empty Optional
         Optional<String> value = Optional.of(str[5]);
+
         // If value is present, it returns an Optional otherwise returns an empty Optional
         System.out.println("Filtered value: "+value.filter((s)->s.equals("Abc")));
         System.out.println("Filtered value: "+value.filter((s)->s.equals("JAVA OPTIONAL CLASS EXAMPLE")));
+
         // It returns value of an Optional. if value is not present, it throws an NoSuchElementException
         System.out.println("Getting value: "+value.get());
+
         // It returns hashCode of the value
         System.out.println("Getting hashCode: "+value.hashCode());
+
         // It returns true if value is present, otherwise false
         System.out.println("Is value present: "+value.isPresent());
+
         // It returns non-empty Optional if value is present, otherwise returns an empty Optional
         System.out.println("Nullable Optional: "+Optional.ofNullable(str[5]));
+
         // It returns value if available, otherwise returns specified value,
         System.out.println("orElse: "+value.orElse("Value is not present"));
         System.out.println("orElse: "+empty.orElse("Value is not present"));
